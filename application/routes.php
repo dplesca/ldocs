c44fbbb966396e70d31039ca11e5770f768dcd4e
Plesca Dragos <plescadragoss@yahoo.com>
2012-11-13 18:44:19 +0200

    Cache the navigation

--- application/routes.php
+++ application/routes.php
@@ -5,7 +5,12 @@ return array(
 	'GET /' => function()
 	{		
 		$parser = IoC::resolve('markdown');
-		$navigation = str_replace('/docs', '', $parser->transform( File::get('../docs/contents.md') ) );
+		if (Cache::has('navigation')){
+			$navigation = Cache::get('navigation');
+		} else {
+			$navigation = str_replace('/docs', '', $parser->transform( File::get('../docs/contents.md') ) );
+			Cache::put('navigation', $navigation, 60*24);
+		}		
 		$content = $parser->transform(File::get('../docs/index.md'));
 		return View::make('home.index')->with('navigation', $navigation)->with('content', $content);
 	},
@@ -13,7 +18,12 @@ return array(
 	'GET /(:any)/(:any)' =>  function($folder, $file){
 		if(File::exists('../docs/'.$folder.'/'.$file.'.md')){
 			$parser = IoC::resolve('markdown');
-			$navigation = str_replace('/docs', '', $parser->transform( File::get('../docs/contents.md') ) );
+			if (Cache::has('navigation')){
+				$navigation = Cache::get('navigation');
+			} else {
+				$navigation = str_replace('/docs', '', $parser->transform( File::get('../docs/contents.md') ) );
+				Cache::put('navigation', $navigation, 60*24);
+			}
 			$content = str_replace('/docs', '', $parser->transform( File::get('../docs/'.$folder.'/'.$file.'.md') ) );
 			return View::make('home.index')->with('navigation', $navigation)->with('content', $content);
 		}
